c2d1bb22eab8dbdb22301a8007246d5835840a3c
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-09-30 11:58:17 -0500

    added-header

---
+++ src/componets/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+      <ul>
+          <li>Home</li>
+          
+      </ul>
+    </div>
+  )
+}
+
+export default Header
